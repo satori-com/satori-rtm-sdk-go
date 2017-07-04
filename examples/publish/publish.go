3c6b3632a02a588fbb2f5d0022875667e2232b7a
Aleksandr Zinovev <azinoviev@satori.com>
2017-07-04 17:02:26 +0200

    Tutorial: Update publish snippet.

--- examples/publish/publish.go
+++ examples/publish/publish.go
@@ -5,39 +5,25 @@ import (
 	"github.com/satori-com/satori-rtm-sdk-go/rtm"
 	"github.com/satori-com/satori-rtm-sdk-go/rtm/auth"
 	"os"
+	"time"
 )
 
 const (
-	// Replace these values with your project's credentials
-	// from Dev Portal (https://developer.satori.com/#/projects).
-	ENDPOINT = "YOUR_ENDPOINT"
-	APP_KEY  = "YOUR_APPKEY"
-
-	// Role and Secret are optional. Leaving these variables as is means no authentication.
+	ENDPOINT        = "YOUR_ENDPOINT"
+	APP_KEY         = "YOUR_APPKEY"
 	ROLE            = "YOUR_ROLE"
 	ROLE_SECRET_KEY = "YOUR_SECRET"
-
-	// Any channel name
-	CHANNEL = "YOUR_CHANNEL"
 )
 
-// We define message struct that we are going to publish
 type Animal struct {
 	Who   string     `json:"who"`
 	Where [2]float32 `json:"where"`
 }
 
 func main() {
-	options := rtm.Options{}
-	if ROLE_SECRET_KEY != "YOUR_SECRET" {
-		// AuthProvider performs additional actions to authenticate the client
-		//
-		// Satori Docs: Authentication
-		// https://www.satori.com/docs/using-satori/authentication
-		authProvider := auth.New(ROLE, ROLE_SECRET_KEY)
-		options = rtm.Options{
-			AuthProvider: authProvider,
-		}
+	authProvider := auth.New(ROLE, ROLE_SECRET_KEY)
+	options := rtm.Options{
+		AuthProvider: authProvider,
 	}
 
 	client, err := rtm.New(ENDPOINT, APP_KEY, options)
@@ -47,31 +33,29 @@ func main() {
 	}
 
 	connected := make(chan bool)
-	// We just created a new client. The client is not connected yet and we will connect it later.
-	// Before start the client you can specify additional callbacks to be able to react on events.
-	// The full events list is specified here: https://godoc.org/github.com/satori-com/satori-rtm-sdk-go/rtm#hdr-EVENTS
 	client.OnConnected(func() {
-		fmt.Println("Connected to RTM!")
+		fmt.Println("Connected to Satori RTM!")
 		connected <- true
 	})
 	client.OnError(func(err rtm.RTMError) {
-		fmt.Println("Error: " + err.Reason.Error())
+		fmt.Println("Failed to connect: " + err.Reason.Error())
 		os.Exit(2)
 	})
 
 	client.Start()
 
-	// Wait for connected state
 	<-connected
+	for {
+		ack := <-client.PublishAck("animals", Animal{
+			Who:   "zebra",
+			Where: [2]float32{34.134358, -118.321506},
+		})
 
-	ack := <-client.PublishAck(CHANNEL, Animal{
-		Who:   "zebra",
-		Where: [2]float32{34.134358, -118.321506},
-	})
-
-	if ack.Err != nil {
-		fmt.Println("Unable to publish: " + ack.Err.Error())
-	} else {
-		fmt.Println("Message has been published successfully. Position: " + ack.Response.Position)
+		if ack.Err != nil {
+			fmt.Println("Failed to publish: " + ack.Err.Error())
+		} else {
+			fmt.Println("Publish confirmed")
+		}
+		time.Sleep(1 * time.Second)
 	}
 }
